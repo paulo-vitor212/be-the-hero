697962e111d8ef498a5c056dd6f4aadba73d8069
Paulo Vitor <paulovitor2123@gmail.com>
2020-03-29 00:59:07 -0300

    Ajustando rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,13 +9,13 @@ const routes = express.Router();
 
 // -------- Rotas de ONGS -------------
 routes.post('/ongs', celebrate({
-    [Segments.BODY]: Joi.object().keys({
+    [Segments.BODY]: Joi.object({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
         whatsapp:Joi.number().required().min(10).max(13),
         city:Joi.string().required(),
         uf:Joi.string().required().length(2),
-    })
+    }).keys()
 }), OngController.create);
 
 routes.get('/ongs', OngController.index);
@@ -23,14 +23,14 @@ routes.get('/ongs', OngController.index);
 routes.delete('/ongs', OngController.delete);
 
 routes.put('/ongs',celebrate({
-    [Segments.BODY]: Joi.object().keys({
+    [Segments.BODY]: Joi.object({
         id: Joi.string().required(),
         name: Joi.string(),
         email: Joi.string().email(),
         whatsapp:Joi.number().min(10).max(13),
         city:Joi.string(),
         uf:Joi.string().length(2),
-    })
+    }).keys()
 }), OngController.update);
 
 
@@ -40,18 +40,18 @@ routes.put('/ongs',celebrate({
 
 
 routes.post('/incidents',celebrate({
-    [Segments.PARAMS]:Joi.object().keys({
+    [Segments.PARAMS]:Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         value: Joi.number().required(),
-    })
+    }).keys()
 }), IncidentController.create);
 
 
 routes.put('/incidents/:id',celebrate({
-    [Segments.PARAMS]:Joi.object().keys({
+    [Segments.PARAMS]:Joi.object({
         id: Joi.number().required()
-    })
+    }).keys()
 }), IncidentController.edit);
 
 
@@ -62,25 +62,25 @@ routes.get('/incidentsOng', IncidentController.indexOng);
 
 
 routes.get('/incidents/:id',celebrate({
-    [Segments.PARAMS]:Joi.object().keys({
+    [Segments.PARAMS]:Joi.object({
         id: Joi.number().required()
-    })
+    }).keys()
 }), IncidentController.show);
 
 
 
 routes.delete('/incidents/:id', celebrate({
-    [Segments.PARAMS]:Joi.object().keys({
+    [Segments.PARAMS]:Joi.object({
         id: Joi.number().required()
-    })
+    }).keys()
 }),IncidentController.delete);
 
 
 
 routes.post('/session',celebrate({
-    [Segments.BODY]: Joi.object().keys({
+    [Segments.BODY]: Joi.object({
         id: Joi.string().required()
-    })
+    }).keys()
 }) ,SessionController.session);
 
 
